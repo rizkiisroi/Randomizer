9ee78fbf0853874e147f09e112d50a1087f4ce1c
rizkiisroi <rizkiisroi@gmail.com>
2020-11-20 11:28:05 +0700

    Initial commit

---
+++ app/src/main/java/com/alam/randomizer/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.alam.randomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+/*import kotlin.random.Random
+ */
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random().nextInt(seekBar.progress) + 1
+            resultsTextView.text = rand.toString()
+        }
+
+    }
+}
